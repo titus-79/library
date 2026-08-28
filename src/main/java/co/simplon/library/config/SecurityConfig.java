f81861de83a5c3cd6bfcf5a3075eec53a7334556
titus-79 <titus@ik.me>
2026-08-28 14:30:02 +0200

    feat: collection bruno

--- src/main/java/co/simplon/library/config/SecurityConfig.java
+++ src/main/java/co/simplon/library/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/**").permitAll())
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()))
                 .formLogin(Customizer.withDefaults())
