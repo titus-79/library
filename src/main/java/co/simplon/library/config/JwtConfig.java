53c679e92af2f813eb54180c7ce392bbd454d236
titus-79 <titus@ik.me>
2026-08-28 10:42:10 +0200

    feat: Asymmetric JWT

---
+++ src/main/java/co/simplon/library/config/JwtConfig.java
@@ -0,0 +1,40 @@
+package co.simplon.library.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.private.key}")
+    RSAPrivateKey privateKey;
+
+    @Value("${jwt.public.key}")
+    RSAPublicKey publicKey;
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+}
--- src/main/java/co/simplon/library/config/SecurityConfig.java
+++ src/main/java/co/simplon/library/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package co.simplon.library.config;
 
 import co.simplon.library.service.AuthService;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,12 +11,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfig {
 
     private final AuthService authService;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     public SecurityConfig (AuthService authServiceInjected) {
         this.authService = authServiceInjected;
@@ -26,6 +37,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/**").permitAll())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()))
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
@@ -36,11 +49,12 @@ public class SecurityConfig {
     }
 
     // Nécessaire pour l'authentification pour les JWT exprime à Spring security quel service il doit utiliser pour authentifier l'utilisateur
-    // une fois déclarer on a le droit de faire une injection de dépendance dans le authcontroller pour la route login
+    // une fois déclaré on a le droit de faire une injection de dépendance dans le authcontroller pour la route login
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authManagerBuilder.userDetailsService(authService);
         return authManagerBuilder.build();
     }
+
 }
