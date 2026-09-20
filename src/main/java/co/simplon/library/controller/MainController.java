017ba588f73b8f829ca78590e8d0d9a255e3b605
titus-79 <titus@ik.me>
2026-09-20 22:37:33 +0200

    feat: autorisations routes

--- src/main/java/co/simplon/library/controller/MainController.java
+++ src/main/java/co/simplon/library/controller/MainController.java
@@ -5,6 +5,7 @@ import co.simplon.library.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -22,26 +23,31 @@ public class MainController {
         this.bookService = bookServiceInjected;
     }
 //    GET /api/books
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping
     public ResponseEntity<List<BookEntity>> getAllBooks() {
         return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
     }
 //    GET /api/books/{id}
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping("/{id}")
     public ResponseEntity<BookEntity> getBookById(@PathVariable UUID id) {
         return new ResponseEntity<>(bookService.getBookById(id), HttpStatus.OK);
     }
 //    POST /api/books
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<BookEntity> createBook(@Valid @RequestBody BookEntity book) {
         return new ResponseEntity<>(bookService.createBook(book), HttpStatus.OK);
     }
 //    PUT /api/books/{id}
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<BookEntity> updateBook(@PathVariable UUID id,@Valid @RequestBody BookEntity book) {
         return new ResponseEntity<>(bookService.updateBook(id, book), HttpStatus.OK);
     }
 //    DELETE /api/books/{id}
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable UUID id) {
         bookService.deleteBook(id);
